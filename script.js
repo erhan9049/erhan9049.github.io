d8b99a5d32f1d0df107c996641ef300084715163
erhan9049 <jamuryumh1@yandex.com>
2018-11-28 16:43:45 +0300

    Add Form

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var coordinatesDisplay = document.getElementById('coordinatesDisplay');
 var accuracyDisplay = document.getElementById('accuracyDisplay');
 var timestampDisplay = document.getElementById('timestampDisplay');
+var saveButton = document.getElementById('saveButton');
+
+var longitudeTextbox = document.getElementById('longitudeTextbox');
+var latitudeTextbox = document.getElementById('latitudeTextbox');
+var nameTextbox = document.getElementById('nameTextbox');
 
 if ("geolocation" in navigator) {
     coordinatesDisplay.innerHTML = 'geolocation is available';
@@ -16,9 +21,11 @@ function geo_success(position) {
     if (Number(position.coords.accuracy) >= 10) {
         console.log('red');
         accuracyDisplay.className = 'redText';
+        fillFormButton.disabled = true;
     } else {
         console.log('green');
         accuracyDisplay.className = 'greenText';
+        fillFormButton.disabled = false;
     }
 }
 
@@ -33,3 +40,12 @@ var geo_options = {
 };
 
 var wpid = navigator.geolocation.watchPosition(geo_success, geo_error, geo_options);
+
+function saveButtonClicked() {
+    alert(nameTextbox.value + '.kolaykonum.com was created for: ' + position.coords.latitude + ', ' + position.coords.longitude);
+}
+
+function fillForm() {
+    longitudeTextbox.value = position.coords.longitude;
+    latitudeTextbox.value = position.coords.latitude;
+}
